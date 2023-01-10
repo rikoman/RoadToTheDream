ff097e8e48eddac1d72f9a329d32ce70b141370a
roman <rikoman70@gmail.com>
2023-01-10 09:14:43 +0700

    clear all

--- backend/src/main/java/com/example/backend/MainTask/Repository/TaskRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.backend.MainTask.Repository;
-
-import com.example.backend.MainTask.Task;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TaskRepository extends MongoRepository<Task, String> {}
